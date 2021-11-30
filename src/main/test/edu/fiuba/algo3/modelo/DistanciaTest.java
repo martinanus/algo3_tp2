c9f70b9d1450545536d79593a04dee821463f6fa
Agustin Rodriguez <agrodriguez@fi.uba.ar>
2021-11-30 18:30:19 -0300

    Se agrega modelo de coordenadas geográficas

---
+++ src/main/test/edu/fiuba/algo3/modelo/DistanciaTest.java
@@ -0,0 +1,34 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DistanciaTest {
+
+    @Test
+    public void test01SeCalculaLaDistanciaEntreBuenosAiresYMadrid() {
+        /* Arrange */
+        Posicion posicionBuenosAires = new Posicion(-34.61305556, -58.37722222);
+        Posicion posicionMadrid = new Posicion(40.41638889, -3.7025);
+
+        /* Act */
+        double distancia = posicionBuenosAires.calcularDistanciaCon(posicionMadrid);
+
+        /* Assert */
+        assertEquals(10000, distancia, 50);
+    }
+
+    @Test
+    public void test02SeCalculaLaDistanciaEntreCiudadDeMexicoYMontreal() {
+        /* Arrange */
+        Posicion posicionMexico = new Posicion(19.42833333, -99.1275);
+        Posicion posicionMontreal = new Posicion(45.50861111, -73.58777778);
+
+        /* Act */
+        double distancia = posicionMexico.calcularDistanciaCon(posicionMontreal);
+
+        /* Assert */
+        assertEquals(3700, distancia, 50);
+    }
+}
