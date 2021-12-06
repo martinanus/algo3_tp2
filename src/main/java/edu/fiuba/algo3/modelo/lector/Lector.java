25cc83f9c3799dbcbce11e09aa9b4d71fd4d132f
SantiC8 <scuretti@fi.uba.ar>
2021-12-05 23:13:46 -0300

    Agrego dependencia org.json..

--- src/main/java/edu/fiuba/algo3/modelo/lector/Lector.java
+++ src/main/java/edu/fiuba/algo3/modelo/lector/Lector.java
@@ -1,16 +1,16 @@
 package edu.fiuba.algo3.modelo.lector;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class Lector {
 
-    public static void cargaDatos() throws IOException, ParseException {
+    public static void cargaDatos() throws IOException, org.json.simple.parser.ParseException {
         // parsing file "JSONExample.json"
         Object ob = new JSONParser().parse(new FileReader(System.getProperty("user.dir") + "/jsons/ciudades.json"));
 
