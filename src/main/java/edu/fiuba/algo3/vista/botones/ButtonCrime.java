beb3e7b8c897a1cbca4d1c0faf0a78dc0c06ac72
SantiC8 <scuretti@fi.uba.ar>
2021-12-21 16:32:30 -0300

    Separacion de vista y controlador

--- src/main/java/edu/fiuba/algo3/vista/botones/ButtonCrime.java
+++ src/main/java/edu/fiuba/algo3/vista/botones/ButtonCrime.java
@@ -1,8 +1,6 @@
 package edu.fiuba.algo3.vista.botones;
 
 import edu.fiuba.algo3.controlador.BotonCrimenHandler;
-import edu.fiuba.algo3.controlador.BotonVerHandler;
-import edu.fiuba.algo3.modelo.AlgoThief;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
