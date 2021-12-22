d357a10d0730e2b5053770eccabb36ac46c77df4
Martin <manus@fi.uba.ar>
2021-12-22 19:56:49 -0300

    finaliza caso-->presenta nuevo vaso

--- src/main/java/edu/fiuba/algo3/controlador/BotonNuevoCasoHandler.java
+++ src/main/java/edu/fiuba/algo3/controlador/BotonNuevoCasoHandler.java
@@ -1,18 +1,19 @@
 package edu.fiuba.algo3.controlador;
 
 import edu.fiuba.algo3.modelo.AlgoThief;
+import edu.fiuba.algo3.vista.ContenedorCaso;
 import edu.fiuba.algo3.vista.ContenedorCiudad;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class BotonVolverAJugarHandler implements EventHandler<ActionEvent> {
+public class BotonNuevoCasoHandler implements EventHandler<ActionEvent> {
 
     private AlgoThief juego;
     private Stage stage;
 
-    public BotonVolverAJugarHandler(Stage stage, AlgoThief juego) {
+    public BotonNuevoCasoHandler(Stage stage, AlgoThief juego) {
         this.stage = stage;
         this.juego = juego;
     }
@@ -20,9 +21,9 @@ public class BotonVolverAJugarHandler implements EventHandler<ActionEvent> {
     @Override
     public void handle(ActionEvent event) {
         juego.generarPartida();
-        ContenedorCiudad contenedorCiudad = new ContenedorCiudad(juego, stage);
-        Scene sceneCiudad = new Scene(contenedorCiudad, 640, 480);
-        stage.setScene(sceneCiudad);
+        ContenedorCaso contenedorCaso = new ContenedorCaso(stage, juego);
+        Scene sceneCaso = new Scene(contenedorCaso, 640, 480);
+        stage.setScene(sceneCaso);
 
 
     }
--- src/main/java/edu/fiuba/algo3/vista/ContenedorCasoFinalizado.java
+++ src/main/java/edu/fiuba/algo3/vista/ContenedorCasoFinalizado.java
@@ -1,17 +1,11 @@
 package edu.fiuba.algo3.vista;
 
-import edu.fiuba.algo3.controlador.BotonInicioHandler;
-import edu.fiuba.algo3.controlador.BotonVolverAJugarHandler;
+import edu.fiuba.algo3.controlador.BotonNuevoCasoHandler;
 import edu.fiuba.algo3.modelo.AlgoThief;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -28,8 +22,8 @@ public class ContenedorCasoFinalizado extends VBox {
 
         Button botonVolverAJugar = new Button();
         botonVolverAJugar.setText("Volver a Jugar");
-        BotonVolverAJugarHandler botonVolverAJugarHandler = new BotonVolverAJugarHandler(stage, juego);
-        botonVolverAJugar.setOnAction(botonVolverAJugarHandler);
+        BotonNuevoCasoHandler botonNuevoCasoHandler = new BotonNuevoCasoHandler(stage, juego);
+        botonVolverAJugar.setOnAction(botonNuevoCasoHandler);
 
         this.getChildren().addAll(titulo, botonVolverAJugar);
         stage.setTitle("Juego - Mision Fallida");
