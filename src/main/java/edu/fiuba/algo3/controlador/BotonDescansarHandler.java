d0962e41a95bf7d18e4210b7dac5e57631b27f94
Martin <manus@fi.uba.ar>
2021-12-23 21:12:16 -0300

    dc caso asignado

---
+++ src/main/java/edu/fiuba/algo3/controlador/BotonDescansarHandler.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.controlador;
+
+import edu.fiuba.algo3.modelo.AlgoThief;
+import javafx.stage.Stage;
+
+public class BotonDescansarHandler {
+    public BotonDescansarHandler(Stage stage, AlgoThief juego) {
+    }
+}
